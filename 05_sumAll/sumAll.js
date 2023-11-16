fee0f3ebff8e0f257426af7a61edd80932e9c56d
Justine Patoux <justine.patoux@gmail.com>
2023-11-16 20:38:26 +0100

    1 to 7 exercices completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if(isNaN(numA) || isNaN(numB)) {
+        return 'ERROR';
+    }
 
+    if (numA < 0 || numB < 0) {
+        return 'ERROR';
+    }
+
+    return (Math.abs(numA - numB) + 1) * (numA + numB) / 2;
 };
 
 // Do not edit below this line
