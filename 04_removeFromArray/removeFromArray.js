271e4d38b32ca8abdc8558954e0c4c46b0899719
Justine Patoux <justine.patoux@gmail.com>
2023-11-16 20:39:47 +0100

    ex 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, el) {
+    let filtered =  arr.filter((key) => {
+        return key !== el;
+    });
+    
+     return filtered;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+
+  return (num - 32 ) / 1.8;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+
+  return (num * 1.8) + 32;
 };
 
 // Do not edit below this line
